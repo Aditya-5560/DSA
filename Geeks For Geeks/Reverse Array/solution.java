ee95af253cad7c043a5b8b620de1b37a3c630b3e
GeekSync <geeksync@users.noreply.github.com>
2026-09-07 12:32:45 +0530

    Solved: Reverse Array (GeeksforGeeks) Sheets: StriverA2Z, LoveBabbar450, GFG160, StriverSDE

---
+++ Geeks For Geeks/Reverse Array/solution.java
@@ -0,0 +1,27 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem: Reverse Array
+ * URL: https://www.geeksforgeeks.org/problems/reverse-an-array/1
+ * Language: Java
+ * Difficulty: Easy
+ * Topics: Bloomberg, Facebook, TCS, Adobe, Google, Infosys, Capgemini, Morgan Stanley
+ * Runtime: 1.1 s
+ * Memory: N/A
+ * Synced: 2026-09-07T07:02:44.437Z
+ */
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int l= 0;
+        int o =0;
+        int r=arr.length-1;
+        while(l<r){
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
