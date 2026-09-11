655b0c7050f007712ad9a2807f5cfa060203c5c8
Aditya Pradip Salunkhe <salunkheaditya5580@gmail.com>
2026-09-11 11:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (83.41%) - LeetSync

--- Leetcode/58-length-of-last-word/length-of-last-word.java
+++ Leetcode/58-length-of-last-word/length-of-last-word.java
@@ -1,9 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int i = s.length() - 1;
+        //Skip all spaces at end
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
+        //Count the word upto next blank space
         int count = 0;
         while (i >= 0 && s.charAt(i) != ' ') {
             count++;
